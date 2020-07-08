e55946abdf9b3d1e57b05e62b6f9b94286adefbf
PaulBoegel <pboegelsack@gmail.com>
2020-07-08 11:05:13 +0200

    Added factory and manager for fullnode api

---
+++ server/fullnodeApiFactory.js
@@ -0,0 +1,40 @@
+const LitecoinNotifyer = require('./notifyer/litecoinNotifyer');
+const LitecoinNodeService = require('./services/litecoinNodeService');
+const RpcClient = require('bitcoind-rpc');
+const zmq = require('zeromq');
+
+function FullnodeApiFactory(config){
+
+  if(typeof config === undefined || config == null)
+    throw new ReferenceError('Config is not defined.');
+
+  const apiArray = [];
+
+  function createApi(blockchainName){
+    if(typeof blockchainName === undefined || blockchainName == null)
+      throw new ReferenceError('No blockchain name defined.');
+
+    switch(blockchainName){
+      case "litecoin":
+        return createLitecoinAPI();
+        break;
+    }
+  }
+
+  function getApiArray(){
+    return apiArray;
+  }
+
+  function createLitecoinAPI(){
+    const litecoinSock = new zmq.Subscriber;
+    const litecoinRpc = new RpcClient(config.litecoin.rpc);
+    const notifyer = new LitecoinNotifyer(config.litecoin.worker, litecoinSock);
+    const service = new LitecoinNodeService(litecoinRpc);
+
+    return { blockchain: "litecoin", notifyer:  notifyer, service: service};
+  }
+
+  return {createApi, getApiArray}
+}
+
+module.exports = FullnodeApiFactory;
---
+++ server/fullnodeApiManager.js
@@ -0,0 +1,43 @@
+const EventEmitter = require('events');
+
+function FullnodeApiManager(config) {
+
+  const apiArray = []
+  const events = new EventEmitter();
+
+  function setApi(api){
+    if(typeof api === undefined || api == null)
+      throw new ReferenceError('api array is not defined.');
+
+    apiArray.push(api);
+  }
+
+  function activateAllNotifyer() {
+    if(apiArray.length == 0)
+      throw 'Service array is empty.';
+
+    apiArray.forEach((api) => {
+      api.notifyer.events.addListener('onNewTransaction', onNewTransaction);
+      api.notifyer.connectToSocket();
+      api.notifyer.subscribeToTransactions();
+    });
+  }
+
+  function activateNotifyer(blockchainName) {
+    if(notifyerArray.length == 0)
+      throw 'Notifyer array is empty.';
+
+    const api = apiArray.find(item => item.blockchain == blockchainName);
+    api.notifyer.connectToSocket();
+    api.notifyer.subscribeToTransactions();
+  }
+
+  function onNewTransaction(transaction, blockchainName){
+    const api = apiArray.find(item => item.blockchain == blockchainName);
+    events.emit('onNewTransaction', transaction, api.service);
+  }
+
+  return { activateAllNotifyer, activateNotifyer, setApi, events }
+}
+
+module.exports = FullnodeApiManager;
