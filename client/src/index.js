54c53b39a016164284e98fcb537249d6fbce4bf1
PaulBoegel <pboegelsack@gmail.com>
2020-08-02 16:37:39 +0200

    Installed jquery and proper.js for bootstrap animations

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./index.css";
 
 render(
   <Router>
