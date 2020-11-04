6adb3eca25fd029f1c54c43983a2651228eea069
PaulBoegel <pboegelsack@gmail.com>
2020-11-04 16:24:24 +0100

    Added sync factory, sync manager and blockrepository to server

--- src/server.js
+++ src/server.js
@@ -2,18 +2,22 @@
 
 const fs = require("fs");
 
-const FullnodeServiceManager = require("./lib/fullnodeServiceManager");
 const FullnodeServiceFactory = require("./lib/fullnodeServiceFactory");
-const FullnodeNotifyerManager = require("./lib/fullnodeNotifyerManager.js");
 const FullnodeNotifyerFactory = require("./lib/fullnodeNotifyerFactory");
+const FullnodeSyncFactory = require("./lib/fullnodeSyncFactory");
+const FullnodeServiceManager = require("./lib/fullnodeServiceManager");
+const FullnodeNotifyerManager = require("./lib/fullnodeNotifyerManager.js");
+const FullnodeSyncManager = require("./lib/fullnodeSyncManager");
 
 const ConfigurationHandler = require("./lib/handler/configurationHandler");
 const TransactionHandler = require("./lib/handler/transactionHandler");
 const TransactionRepository = require("./lib/repos/transactionRepository");
+const BlockRepository = require("./lib/repos/blockRepository");
 
 const configHandler = new ConfigurationHandler(fs);
 const config = configHandler.readAndParseJsonFile("./explorer-config.json");
 const transactionRepo = new TransactionRepository(config.dbConfig.test);
+const blockRepo = new BlockRepository(config.dbConfig.test);
 const transactionHandler = new TransactionHandler(transactionRepo);
 
 const fullnodeServiceFactory = new FullnodeServiceFactory(
@@ -26,19 +30,25 @@ const fullnodeNotifyerFactory = new FullnodeNotifyerFactory(
 const fullnodeServiceManager = new FullnodeServiceManager();
 const fullnodeNotifyerManager = new FullnodeNotifyerManager();
 
-fullnodeServiceManager.setService(
-  fullnodeServiceFactory.createService("litecoin")
-);
-fullnodeNotifyerManager.setNotifyer(
-  fullnodeNotifyerFactory.createNotifyer("litecoin")
-);
+fullnodeServiceManager.setService(fullnodeServiceFactory.create("litecoin"));
+fullnodeNotifyerManager.setNotifyer(fullnodeNotifyerFactory.create("litecoin"));
 
-fullnodeServiceManager.setService(
-  fullnodeServiceFactory.createService("bitcoin")
-);
-fullnodeNotifyerManager.setNotifyer(
-  fullnodeNotifyerFactory.createNotifyer("bitcoin")
-);
+fullnodeServiceManager.setService(fullnodeServiceFactory.create("bitcoin"));
+fullnodeNotifyerManager.setNotifyer(fullnodeNotifyerFactory.create("bitcoin"));
+
+const fullnodeSyncFactory = new FullnodeSyncFactory({
+  fullnodeServiceManager,
+  transactionRepo,
+  blockRepo,
+});
+
+const fullnodeSyncManager = new FullnodeSyncManager({
+  fullnodeServiceManager,
+  transactionRepo,
+  blockRepo,
+});
+
+fullnodeSyncManager.setSynchronizer(fullnodeSyncFactory.create("litecoin"));
 
 fullnodeNotifyerManager.events.addListener(
   "onNewTransaction",
@@ -60,4 +70,4 @@ fullnodeServiceManager.events.addListener(
 );
 
 fullnodeServiceManager.activateAllListeners();
-// fullnodeNotifyerManager.activateAllNotifyers();
+fullnodeSyncManager.activateAllSynchronizer();
