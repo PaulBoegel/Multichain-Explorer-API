78a781c2857c3e0c0ffb490642a2a1c78095109f
PaulBoegel <pboegelsack@gmail.com>
2020-11-04 16:26:10 +0100

    fixed LitecoinSync call in sync factory

--- src/lib/fullnodeSyncFactory.js
+++ src/lib/fullnodeSyncFactory.js
@@ -1,15 +1,18 @@
 const LitecoinSync = require("./sync/litecoinSync");
 
-function FullnodeSyncFactory({ serviceManager, transRepo, blockRepo }) {
+function FullnodeSyncFactory({
+  fullnodeServiceManager,
+  transactionRepo,
+  blockRepo,
+}) {
   function create(chainname) {
     switch (chainname) {
       case "litecoin":
-        return new LitecoinSync(
-          serviceManager.getService(chainname),
-          transRepo,
-          blockRepo
-        );
-        break;
+        return new LitecoinSync({
+          service: fullnodeServiceManager.getService(chainname),
+          transRepo: transactionRepo,
+          blockRepo,
+        });
     }
   }
 
