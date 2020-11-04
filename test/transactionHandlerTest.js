92355eab9c6d6cd0fc3f6347585139f84502aebc
PaulBoegel <pboegelsack@gmail.com>
2020-11-04 22:16:38 +0100

    added transaction handler test cases

---
+++ test/transactionHandlerTest.js
@@ -0,0 +1,47 @@
+const assert = require("assert");
+const sinon = require("sinon");
+const { RPCClient } = require("rpc-bitcoin");
+const LitecoinNodeService = require("../src/lib/services/litecoinNodeService");
+const TransactionHandler = require("../src/lib/handler/transactionHandler");
+
+function getRpcConfig() {
+  return {
+    url: "http://127.0.0.1",
+    user: "pboegelsack",
+    pass: "test",
+    port: "8332",
+  };
+}
+
+describe("TransactionHandler saveBlockTransactions", () => {
+  const blockRepo = {
+    connect: () => {},
+    add: (block) => {},
+  };
+  const transRepo = {
+    connect: () => {},
+    addMany: (transaction) => {
+      return transaction.length;
+    },
+  };
+  const rpc = new RPCClient(getRpcConfig());
+  const service = new LitecoinNodeService(rpc, "litecoin");
+  const addBlock = sinon.spy(blockRepo, "add");
+  const addTransaction = sinon.spy(transRepo, "addMany");
+  it("should save requested block data", async () => {
+    const blockhash =
+      "4dae7c13977c2e6b776221e6e7f1bd738f3d5d8cabf40a60f5cbb5b524d4d6bd";
+
+    const handler = new TransactionHandler(transRepo, blockRepo);
+    result = await handler.saveBlockTransactions(blockhash, service);
+    assert(
+      addBlock.withArgs({
+        height: 1941194,
+        hash: blockhash,
+        tx: [
+          "159cc2d17066824b68b3a182e566476b25d54a184061369cd080b5e25bf5280a",
+        ],
+      }).calledOnce
+    );
+  });
+});
