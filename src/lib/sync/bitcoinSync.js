5b2cdc20d2471a4ad640202cb32f332a53a77810
PaulBoegel <pboegelsack@gmail.com>
2020-11-09 11:22:42 +0100

    added chainname to block add

--- src/lib/sync/bitcoinSync.js
+++ src/lib/sync/bitcoinSync.js
@@ -22,6 +22,7 @@ function BitcoinSync({ service, transRepo, blockRepo }) {
         tx: tx.map((transaction) => {
           return transaction.txid;
         }),
+        chainname: CHAINNAME,
       });
       tx.map((transaction) => (transaction.chainname = CHAINNAME));
       inserted += await transRepo.addMany(tx);
