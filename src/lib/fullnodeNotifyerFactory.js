2111ded35e9d9a8b18292c7d33ccb68f78304aaf
PaulBoegel <pboegelsack@gmail.com>
2020-11-09 11:38:19 +0100

    modifie notifyer factory for dash

--- src/lib/fullnodeNotifyerFactory.js
+++ src/lib/fullnodeNotifyerFactory.js
@@ -3,15 +3,17 @@
 const zmq = require("zeromq");
 const LitecoinNotifyer = require("./notifyer/litecoinNotifyer");
 const BitcoinNotifyer = require("./notifyer/bitcoinNotifyer");
+const DashNotifyer = require("./notifyer/dashNotifyer");
 
 function FullnodeNotifyerFactory(config) {
   function create(blockchainName) {
     switch (blockchainName) {
       case config.litecoin.chainname:
         return createLitecoinNotifyer();
-        break;
       case config.bitcoin.chainname:
         return createBitcoinNotifyer();
+      case config.dash.chainname:
+        return createDashNotifyer();
     }
   }
 
@@ -23,6 +25,11 @@ function FullnodeNotifyerFactory(config) {
   function createBitcoinNotifyer() {
     const bitcoinSock = new zmq.Subscriber();
     return new BitcoinNotifyer(config.bitcoin, bitcoinSock);
+  }
+
+  function createDashNotifyer() {
+    const dashSock = new zmq.Subscriber();
+    return new DashNotifyer(config.dash, dashSock);
   }
 
   return { create };
