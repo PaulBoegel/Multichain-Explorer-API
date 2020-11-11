3acfa2e038945769d020e80979880ed86b3a7968
PaulBoegel <pboegelsack@gmail.com>
2020-11-11 20:44:11 +0100

    added transaction output route

--- src/api/routes/transactionRouter.js
+++ src/api/routes/transactionRouter.js
@@ -2,6 +2,7 @@
 
 function TransactionRouter(router, ctrl) {
   router.route("/transactions/:chainname/:txid").get(ctrl.getByTxId);
+  router.route("/transactions/output/:chainname/:txid").get(ctrl.getOutput);
 
   return router;
 }
