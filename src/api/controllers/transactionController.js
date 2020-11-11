ab069a672761da3465be74f5dbba7bc4587a8f78
PaulBoegel <pboegelsack@gmail.com>
2020-11-11 20:43:55 +0100

    added getOutput function

--- src/api/controllers/transactionController.js
+++ src/api/controllers/transactionController.js
@@ -20,7 +20,35 @@ function TransactionController(transactionRepo) {
     }
   }
 
-  return { getByTxId };
+  async function getOutput(req, res) {
+    try {
+      const chainname = req.params.chainname;
+      const txid = req.params.txid;
+      const query = { chainname, "vin.txid": txid };
+      const projection = { _id: 0 };
+      const transaction = await transactionRepo.getByIds(txid, chainname);
+      const outputs = transaction.vout.map((output) => {
+        return { n: output.n, address: output.scriptPubKey.addresses[0] };
+      });
+      const result = await transactionRepo.get(query, projection);
+      if (result) {
+        result.forEach((transaction) => {
+          const input = transaction.vin.find((input) => (input = txid));
+          const address = outputs.find((output) => output.n === input.vout)
+            .address;
+          transaction.address = address;
+        });
+        res.setHeader("Conent-Type", "application/json");
+        res.send(JSON.stringify(result, null, 4));
+        return;
+      }
+    } catch (err) {
+      res.send(err.message);
+    }
+    return res.code(404);
+  }
+
+  return { getByTxId, getOutput };
 }
 
 module.exports = TransactionController;
