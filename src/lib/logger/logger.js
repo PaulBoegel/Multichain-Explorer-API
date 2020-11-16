34fa98766207aed7c2ce53286a4ce693dfb5d6a5
PaulBoegel <pboegelsack@gmail.com>
2020-11-16 15:10:18 +0100

    added logger.js

---
+++ src/lib/logger/logger.js
@@ -0,0 +1,24 @@
+const winston = require("winston");
+const { format, config } = require("winston");
+const { combine, timestamp, json } = format;
+
+class Logger {
+  constructor(options) {
+    return new winston.createLogger({
+      levels: config.syslog.levels,
+      format: combine(
+        timestamp({
+          format: options.dateFormat,
+        }),
+        json()
+      ),
+      transports: [
+        new winston.transports.File(options.file),
+        new winston.transports.Console(options.console),
+      ],
+      exitOnError: false,
+    });
+  }
+}
+
+module.exports = Logger;
