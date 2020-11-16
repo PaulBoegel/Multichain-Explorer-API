7a6b7de2383e1046057a9d4eb761333db36291d0
PaulBoegel <pboegelsack@gmail.com>
2020-11-16 15:16:28 +0100

    deleted hashtag for private property

--- src/lib/logger/blockLogger.js
+++ src/lib/logger/blockLogger.js
@@ -1,15 +1,15 @@
 const Logger = require("./logger");
 
 class BlockLogger {
-  static #INSTANTS;
+  static INSTANTS;
   constructor(options) {
-    if (typeof BlockLogger.#INSTANTS === "object") return;
-    BlockLogger.#INSTANTS = new Logger(options);
+    if (typeof BlockLogger.INSTANTS === "object") return;
+    BlockLogger.INSTANTS = new Logger(options);
   }
 
   static info({ message, data }) {
-    if (typeof BlockLogger.#INSTANTS === "object")
-      return BlockLogger.#INSTANTS.info(message, data);
+    if (typeof BlockLogger.INSTANTS === "object")
+      return BlockLogger.INSTANTS.info(message, data);
 
     throw new Error("logger not instantiated");
   }
