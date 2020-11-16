319089a2de7165325fc18383e208424494b99cd1
PaulBoegel <pboegelsack@gmail.com>
2020-11-16 15:32:47 +0100

    modified logger

--- src/lib/logger/errorLogger.js
+++ src/lib/logger/errorLogger.js
@@ -1,15 +1,15 @@
 const Logger = require("./logger");
 
 class ErrorLogger {
-  static INSTANTS = null;
+  static #INSTANTS = null;
   constructor(options) {
-    if (typeof ErrorLogger.INSTANTS === "object") return;
-    ErrorLogger.INSTANTS = new Logger(options);
+    if (typeof ErrorLogger.#INSTANTS === "object") return;
+    ErrorLogger.#INSTANTS = new Logger(options);
   }
 
   static error(message) {
-    if (typeof ErrorLogger.INSTANTS === "object")
-      return ErrorLogger.INSTANTS.error(message);
+    if (typeof ErrorLogger.#INSTANTS === "object")
+      return ErrorLogger.#INSTANTS.error(message);
 
     throw new Error("logger not instantiated");
   }
