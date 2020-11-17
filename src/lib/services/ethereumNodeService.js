56c762b70a73c34f34057137f46a11269a0019c8
PaulBoegel <pboegelsack@gmail.com>
2020-11-17 13:40:23 +0100

    created new test cases for litecoin and ethereum

---
+++ src/lib/services/ethereumNodeService.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const EventEmitter = require("events");
+
+function EthereumNodeService(rpc, chainname) {
+  const events = new EventEmitter();
+
+  async function decodeTransaction(byteArray) {}
+
+  async function getBlockHash({ height }) {}
+
+  async function getBlock({ blockhash, verbose }) {}
+
+  async function getBlockchainInfo() {}
+
+  async function getTransaction({ txid, verbose = false }) {}
+
+  async function handleTransactionInputs(transaction, depth) {}
+
+  return {
+    decodeTransaction,
+    getTransaction,
+    getBlockchainInfo,
+    getBlock,
+    getBlockHash,
+    handleTransactionInputs,
+    chainname,
+    events,
+  };
+}
+
+module.exports = BitcoinNodeService;
---
+++ test/litecoin-node-tests/litecoinSyncTest.js
@@ -0,0 +1,60 @@
+const assert = require("assert");
+const LitecoinSync = require("../src/lib/sync/litecoinSync");
+const TransactionHandler = require("../src/lib/handler/transactionHandler");
+const sinon = require("sinon");
+const blockMock = require("./mocks/blockMock.json");
+
+describe("LitecoinSync Blockrange", () => {
+  let service, transRepo, blockRepo, sync;
+
+  beforeEach(() => {
+    service = {
+      chainname: "litecoin",
+      getBlockchainInfo: function () {
+        return 1;
+      },
+      getBlockHash: function ({ height }) {
+        return blockMock.hash;
+      },
+      getBlock: function ({ hash }) {
+        return blockMock;
+      },
+    };
+    transRepo = {
+      connect: function () {
+        return true;
+      },
+      addMany: function (transactions) {
+        return transactions.length;
+      },
+    };
+    blockRepo = {
+      connect: function () {
+        return true;
+      },
+      add: function (block) {
+        return true;
+      },
+      get: function (blockParam) {
+        return [
+          {
+            height: 0,
+            hash:
+              "12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2",
+          },
+        ];
+      },
+    };
+    getBlockchainInfo = sinon.spy(service, "getBlockchainInfo");
+    const transactionHandler = TransactionHandler(transRepo, blockRepo);
+    sync = LitecoinSync({ service, transactionHandler });
+  });
+
+  it("should fire blockchainSynchronized event, after end height was reached", (done) => {
+    sync.events.addListener("blockchainSynchronized", (chainname) => {
+      assert(chainname, "litecoin");
+      done();
+    });
+    sync.blockrange().then((result) => {});
+  });
+});
