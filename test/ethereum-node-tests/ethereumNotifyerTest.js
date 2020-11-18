e8d07c635c5eb0f7d42046fa5cd8b757a6ca65c7
PaulBoegel <pboegelsack@gmail.com>
2020-11-18 17:44:41 +0100

    added ethereum notifyer test cases

---
+++ test/ethereum-node-tests/ethereumNotifyerTest.js
@@ -0,0 +1,19 @@
+const { blockchainConfig } = require("../../explorer-config.json");
+const conf = blockchainConfig.ethereum;
+const EthereumNotifyer = require("../../src/lib/notifyer/ethereumNotifyer");
+
+describe("EthereumNotifyer subscribeToBlocks", () => {
+  notifyer = EthereumNotifyer(conf);
+  notifyer.connectToSocket().then();
+  notifyer.subscribeToBlocks().then();
+  it.only("should send the block header if the blockchain creates a new block", (done) => {
+    let isDone = false;
+    notifyer.events.addListener("onNewBlock", (block, chainname) => {
+      if (isDone == false) {
+        assert.strictEqual(block, null);
+        done();
+        isDone = true;
+      }
+    });
+  });
+});
