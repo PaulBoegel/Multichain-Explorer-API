84a0879b9b26963b4e3f1be31922a0a2ee77c14a
PaulBoegel <pboegelsack@gmail.com>
2020-11-19 17:34:35 +0100

    extract logic to bitcoin base function

--- src/lib/services/bitcoinNodeService.js
+++ src/lib/services/bitcoinNodeService.js
@@ -1,80 +1,22 @@
 "use strict";
-
-const EventEmitter = require("events");
-
 function BitcoinNodeService(rpc, chainname) {
-  async function decodeTransaction(byteArray) {
-    try {
-      const transHex = byteArray.toString("hex");
-      const decoded = await rpc.decoderawtransaction({ hexstring: transHex });
-
-      if (coinbaseCheck(decoded)) {
-        decoded.vin = [];
-
-        return decoded;
-      }
-
-      return decoded;
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async function getBlockHash({ height }) {
-    return await rpc.getblockhash({ height });
-  }
-
-  async function getBlock({ blockhash, verbose }) {
-    const { height, hash, tx, nextblockhash } = await rpc.getblock({
-      blockhash,
-      verbosity: verbose ? 2 : 1,
-    });
-    return { height, hash, tx, nextblockhash };
-  }
-
-  async function getBlockchainInfo() {
-    return await rpc.getblockchaininfo();
-  }
-
-  async function getTransaction({ txid, verbose = false }) {
-    return await rpc.getrawtransaction({ txid, verbose });
-  }
-
-  async function handleTransactionInputs(transaction, depth) {
-    try {
-      const inputs = [];
-      const vin = transaction.vin;
-
-      if (vin.length == 0) return;
-
-      if (depth == 0) return;
-
-      depth--;
-
-      for (let i = 0; i < vin.length; i++) {
-        inputs.push(await getTransaction(vin[i].txid, true));
-      }
-
-      events.emit("onNewInputs", inputs, depth, chainname);
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  function coinbaseCheck(transaction) {
-    if (transaction.vin[0].coinbase == undefined) return false;
-
-    return true;
-  }
-
   return {
-    decodeTransaction,
-    getTransaction,
-    getBlockchainInfo,
-    getBlock,
-    getBlockHash,
-    handleTransactionInputs,
-    chainname,
+    async getTransaction({ txid, verbose = false }) {
+      return await rpc.getrawtransaction({ txid, verbose });
+    },
+    async getBlockchainInfo() {
+      return await rpc.getblockchaininfo();
+    },
+    async getBlock({ blockhash, verbose }) {
+      const { height, hash, tx, nextblockhash } = await rpc.getblock({
+        blockhash,
+        verbosity: verbose ? 2 : 1,
+      });
+      return { height, hash, tx, nextblockhash };
+    },
+    async getBlockHash({ height }) {
+      return await rpc.getblockhash({ height });
+    },
   };
 }
 
--- src/lib/services/litecoinNodeService.js
+++ src/lib/services/litecoinNodeService.js
@@ -1,53 +1,11 @@
 "use strict";
-
-const EventEmitter = require("events");
+const BitcoinNodeService = require("./bitcoinNodeService");
 
 function LitecoinNodeService(rpc, chainname) {
-  async function decodeTransaction(byteArray) {
-    try {
-      const transHex = byteArray.toString("hex");
-      const decoded = await rpc.decoderawtransaction({ hexstring: transHex });
-
-      if (coinbaseCheck(decoded)) {
-        decoded.vin = [];
-
-        return decoded;
-      }
-
-      return decoded;
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async function getBlockHash({ height }) {
-    return await rpc.getblockhash({ height });
-  }
-
-  async function getBlock({ blockhash, verbose }) {
-    const { height, hash, tx, nextblockhash } = await rpc.getblock({
-      blockhash,
-      verbosity: verbose ? 2 : 1,
-    });
-    return { height, hash, tx, nextblockhash };
-  }
-
-  async function getBlockchainInfo() {
-    return await rpc.getblockchaininfo();
-  }
-
-  async function getTransaction({ txid, verbose = false }) {
-    return await rpc.getrawtransaction({ txid, verbose });
-  }
-
-  return {
-    decodeTransaction,
-    getTransaction,
-    getBlockchainInfo,
-    getBlock,
-    getBlockHash,
-    chainname,
-  };
+  return Object.setPrototypeOf(
+    Object.assign(BitcoinNodeService(rpc, chainname), {}),
+    BitcoinNodeService
+  );
 }
 
 module.exports = LitecoinNodeService;
