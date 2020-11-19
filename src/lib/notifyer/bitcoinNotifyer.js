976f319ed463a0d86a22b1d4a08f04b1247d7123
PaulBoegel <pboegelsack@gmail.com>
2020-11-19 17:34:11 +0100

    extract notifyer logic to bitcoin base class

--- src/lib/notifyer/bitcoinNotifyer.js
+++ src/lib/notifyer/bitcoinNotifyer.js
@@ -1,42 +1,29 @@
 "use strict";
-
 const EventEmitter = require("events");
-
 function BitcoinNotifyer(conf, sock) {
-  const events = new EventEmitter();
   const relationDepth = conf.transactions.notifyerRelationDepth;
-  const blockchain = "bitcoin";
-
-  async function connectToSocket() {
-    await sock.connect(`tcp://${conf.worker.host}:${conf.worker.port}`);
-    console.log(`${blockchain} worker connected to port ${conf.worker.port}`);
-  }
-
-  async function closeConnection() {
-    return await sock.close();
-  }
-
-  async function subscribeToBlocks() {
-    sock.subscribe("hashblock");
-    for await (const [topic, msg] of sock) {
-      events.emit("onNewBlock", msg.toString("hex"), conf.chainname);
-    }
-  }
-
-  async function subscribeToTransactions() {
-    sock.subscribe("rawtx");
-    for await (const [topic, msg] of sock) {
-      events.emit("onNewTransaction", msg, relationDepth, conf.chainname);
-    }
-  }
-
   return {
-    connectToSocket,
-    closeConnection,
-    subscribeToTransactions,
-    subscribeToBlocks,
+    events: new EventEmitter(),
     blockchain,
-    events,
+    async connectToSocket() {
+      await sock.connect(`tcp://${conf.worker.host}:${conf.worker.port}`);
+      console.log(`${blockchain} worker connected to port ${conf.worker.port}`);
+    },
+    async closeConnection() {
+      return await sock.close();
+    },
+    async subscribeToBlocks() {
+      sock.subscribe("hashblock");
+      for await (const [topic, msg] of sock) {
+        events.emit("onNewBlock", msg.toString("hex"), conf.chainname);
+      }
+    },
+    async subscribeToTransactions() {
+      sock.subscribe("rawtx");
+      for await (const [topic, msg] of sock) {
+        events.emit("onNewTransaction", msg, relationDepth, conf.chainname);
+      }
+    },
   };
 }
 
--- src/lib/notifyer/litecoinNotifyer.js
+++ src/lib/notifyer/litecoinNotifyer.js
@@ -1,43 +1,10 @@
 "use strict";
-
-const EventEmitter = require("events");
-
+BitcoinNotifyer = require("./bitcoinNotifyer");
 function LitecoinNotifyer(conf, sock) {
-  const events = new EventEmitter();
-  const relationDepth = conf.transactions.notifyerRelationDepth;
-  const blockchain = "litecoin";
-
-  async function connectToSocket() {
-    await sock.connect(`tcp://${conf.worker.host}:${conf.worker.port}`);
-    console.log(`${blockchain} worker connected to port ${conf.worker.port}`);
-  }
-
-  async function closeConnection() {
-    return await sock.close();
-  }
-
-  async function subscribeToBlocks() {
-    sock.subscribe("hashblock");
-    for await (const [topic, msg] of sock) {
-      events.emit("onNewBlock", msg.toString("hex"), conf.chainname);
-    }
-  }
-
-  async function subscribeToTransactions() {
-    sock.subscribe("rawtx");
-    for await (const [topic, msg] of sock) {
-      events.emit("onNewTransaction", msg, relationDepth, conf.chainname);
-    }
-  }
-
-  return {
-    connectToSocket,
-    closeConnection,
-    subscribeToTransactions,
-    subscribeToBlocks,
-    blockchain,
-    events,
-  };
+  return Object.setPrototypeOf(
+    Object.assign(BitcoinNotifyer(conf, sock), {}),
+    BitcoinNotifyer
+  );
 }
 
 module.exports = LitecoinNotifyer;
