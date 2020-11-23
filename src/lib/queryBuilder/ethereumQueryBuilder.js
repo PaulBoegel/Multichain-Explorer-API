28f22625a572da826c40c8005d6fd38569119d4c
PaulBoegel <pboegelsack@gmail.com>
2020-11-23 19:58:19 +0100

    modified

--- src/lib/queryBuilder/ethereumQueryBuilder.js
+++ src/lib/queryBuilder/ethereumQueryBuilder.js
@@ -1,9 +1,21 @@
-function EthereumQueryBuilder(formater) {
+function EthereumQueryBuilder(formater, repo) {
   const queryBuilder = {
-    addressSearchQuery(address) {
-      return {
-        //"vout.address": address,
-      };
+    async addressSearchQuery(address) {
+      const toQuery = { to: address };
+      const fromQuery = { from: address };
+      const projection = { _id: 0 };
+      const transactions = [];
+
+      await this.repo.connect();
+
+      transactions.push(...(await this.repo.get(toQuery, projection)));
+      transactions.push(...(await this.repo.get(fromQuery, projection)));
+
+      for (let transaction of transactions) {
+        await this.formater.formatAccountStructure(transaction);
+      }
+
+      return transactions;
     },
   };
 
@@ -14,9 +26,16 @@ function EthereumQueryBuilder(formater) {
     configurable: true,
   });
 
-  Object.defineProperty(queryBuilder, formater, {
+  Object.defineProperty(queryBuilder, "formater", {
     value: formater,
     writable: true,
+    enumerable: true,
+    configurable: true,
+  });
+
+  Object.defineProperty(queryBuilder, "repo", {
+    value: repo,
+    writable: false,
     enumerable: true,
     configurable: true,
   });
