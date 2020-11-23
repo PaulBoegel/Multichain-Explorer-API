f7f9f82b30eaf1e2e7ae2e9d57f327895d45e11f
PaulBoegel <pboegelsack@gmail.com>
2020-11-23 16:06:47 +0100

    added formater to getOutput

--- src/api/controllers/transactionController.js
+++ src/api/controllers/transactionController.js
@@ -24,28 +24,14 @@ function TransactionController(transactionRepo, formaterManager) {
     try {
       const chainname = req.params.chainname;
       const txid = req.params.txid;
-      const query = { chainname, "vin.txid": txid };
-      const projection = { _id: 0 };
       const transaction = await transactionRepo.getByIds(txid, chainname);
-      const outputs = transaction.vout.map((output) => {
-        return { n: output.n, address: output.scriptPubKey.addresses[0] };
-      });
-      const result = await transactionRepo.get(query, projection);
-      if (result) {
-        result.forEach((transaction) => {
-          const input = transaction.vin.find((input) => (input = txid));
-          const { address, n } = outputs.find(
-            (output) => output.n === input.vout
-          );
-          transaction.to = transaction.vout.find(
-            (entry) => entry.n === n
-          ).scriptPubKey.addresses[0];
-          transaction.from = address;
-        });
-        res.setHeader("Conent-Type", "application/json");
-        res.send(JSON.stringify(result, null, 4));
-        return;
-      }
+      const formater = formaterManager.getFormater(chainname);
+      const formatedTransaction = await formater.formatAccountStructure(
+        transaction,
+        transactionRepo
+      );
+      res.setHeader("Conent-Type", "application/json");
+      res.send(JSON.stringify(formatedTransaction, null, 4));
     } catch (err) {
       res.send(err.message);
     }
