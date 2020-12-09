2cf576c4be71f0198d43b8cb32c33190c98d2873
PaulBoegel <pboegelsack@gmail.com>
2020-12-09 22:39:50 +0100

    update

--- src/lib/sync/bitcoinSync.js
+++ src/lib/sync/bitcoinSync.js
@@ -89,7 +89,7 @@ function BitcoinSync({
 
         if (blockData.height - 1 === lastHeightSaved || lastHeightSaved === 0) {
           const saveTime = await transactionHandler.saveBlockData(blockData);
-          console.log("gespeichert ohne cache");
+          console.log(chainId + "gespeichert ohne cache");
           console.log("neue Höhe: " + lastHeightSaved);
           lastHeightSaved = blockData.height;
           // BlockLogger.info({
